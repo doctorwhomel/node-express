99e99fd452d3c7f82d181bf56ecc045c740a077d
WHOMEL <williamhomel@gmail.com>
2022-11-05 13:06:13 -0700

    workshop 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -3,7 +3,7 @@ const campsiteRouter = express.Router();
 
 campsiteRouter
   .route("/")
-  .all((rec, res, next) => {
+  .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     next();
@@ -22,6 +22,32 @@ campsiteRouter
   })
   .delete((req, res) => {
     res.end("Deleting all campsites");
+  });
+
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(
+      `Will send details of the campsite: ${req.params.campsiteId} to you`
+    );
+  })
+  .post((req, res) => {
+    res.end(
+      `POST operation not supported on /campsites/${req.params.campsiteId}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+  })
+  .delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
   });
 
 module.exports = campsiteRouter;
